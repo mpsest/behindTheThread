2dac5c59761339521f42340514748766b58a1374
mpsest <mpsest6@gmail.com>
2026-08-12 16:31:52 +0100

    RootLayout jsx e css: navbar e menu completos; 4 colunas no footer, imagem com titulo.

--- frontend/src/components/RootLayout.jsx
+++ frontend/src/components/RootLayout.jsx
@@ -5,15 +5,58 @@ export default function RootLayout() {
     <div>
       <nav className="navbar navbar-expand-lg">
         <div className="container">
-          <div>Menu</div>
-          <h4>BEHIND THE THREAD</h4>
-          <div>Contactos</div>
+          <div class="dropdown-center">
+            <button
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Menu
+            </button>
+            <ul class="dropdown-menu">
+              <li>
+                <a class="dropdown-item" href="#">
+                  DIRTY TALKS
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  ARTIGOS
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  DESIGNERS
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  BASE DE DADOS
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  MISTURAS
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  SORTUDOS
+                </a>
+              </li>
+            </ul>
+          </div>
+          <h1>BEHIND THE THREAD</h1>
+          <button>Contactos</button>
         </div>
       </nav>
       <main></main>
       <footer className="site-footer">
         <div className="container site-footer-content">
-          <small>Behind the Thread</small>
+          <img
+            src="/btt-footer.svg"
+            alt="imagem com o título Behind the Thread"
+          ></img>
           <div>menu</div>
           <div>social media</div>
           <div>newsletter</div>
