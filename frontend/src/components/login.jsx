8bf6e6f7ab35cddf5d3cffe4bec4c1fc49f256af
barbaracruz5 <cruzbarbara95@gmail.com>
2026-08-21 16:21:23 +0200

    component login

---
+++ frontend/src/components/login.jsx
@@ -0,0 +1,30 @@
+
+import {SquareButton} from './SquareButton.jsx';
+
+export default function Login() {
+
+  function printHello(name) {
+    alert("hi! "+name);
+  }
+
+  return (
+    <form>
+      <h2>Login</h2>
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password" />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <ComponentButton functionForClick={()=>printHello('bla')}>Login</ComponentButton>
+      </p>
+    </form>
+  );
+}
