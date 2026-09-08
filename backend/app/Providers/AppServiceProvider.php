3aa01a884149a917669f3d50d551eb32d8ea8add
Filipe Cruz <inf.fcruz@gmail.com>
2026-09-08 16:40:16 +0100

    Apaguei o código morto do AppServiceProvider.php

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,25 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Mail;
-use Symfony\Component\Mailer\Bridge\Mailtrap\Transport\MailtrapTransportFactory;
-use Symfony\Component\Mailer\Transport\Dsn;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
 
-public function boot(): void
-{
-    Mail::extend('mailtrap', function () {
-        $factory = new MailtrapTransportFactory();
-
-        $dsn = Dsn::fromString(sprintf(
-            'mailtrap+sandbox://%s@default?inboxId=%s',
-            config('services.mailtrap.token'),
-            config('services.mailtrap.inbox_id')
-        ));
-
-        return $factory->create($dsn);
-    });
-}
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
 }
