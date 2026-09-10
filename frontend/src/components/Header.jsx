cf11239125f9ffd857f3785c1fd88bd324008d97
mpsest <mpsest6@gmail.com>
2026-09-10 18:02:58 +0100

    Fix nas margens da navbar.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,15 +5,13 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <nav className="navbar">
-      <div className="container d-flex align-items-center justify-content-between gap-3">
-        <Menu />
-        <h1>
-          <Link to="/">BEHIND THE THREAD</Link>
-        </h1>
-        <Link className="header-contact-link" to="/contactos">
-          Contactos
-        </Link>
-      </div>
+      <Menu />
+      <h1>
+        <Link to="/">BEHIND THE THREAD</Link>
+      </h1>
+      <Link className="header-contact-link" to="/contactos">
+        Contactos
+      </Link>
     </nav>
   );
 }
